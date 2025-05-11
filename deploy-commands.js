2069c37ba5f49743553eb1d064fa00473fc7b7e8
Justin Henry <jhh2524@ftr.com>
2025-05-11 16:02:48 -0500

    remove ping

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,11 +1,10 @@
 import { REST, Routes } from 'discord.js'
-import ping from './commands/ping.js'
 import requestTour from './commands/request-tour.js'
 
 import dotenv from 'dotenv'
 dotenv.config()
 
-const commands = [ping.data.toJSON(), requestTour.data.toJSON()]
+const commands = [requestTour.data.toJSON()]
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(process.env.DISCORD_TOKEN)
