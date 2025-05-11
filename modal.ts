09c3e7c21c0a4ce208b386945fdc26bb62bccd92
Justin Henry <jhh2524@ftr.com>
2025-05-11 00:17:07 -0500

    added get to interactions url

--- modal.ts
+++ modal.ts
@@ -112,7 +112,9 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), function 
     }
   }
 });
-
+app.get('/interactions', (req, res) => {
+  res.send('Hello World!');
+});
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
